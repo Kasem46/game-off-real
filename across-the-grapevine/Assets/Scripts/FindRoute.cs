99db93d74f76fcbd1d9b2e16027e46e401dc048a
Dexo55 <Dexoh53@gmail.com>
2022-11-13 20:29:22 -0500

    Did something

--- across-the-grapevine/Assets/Scripts/FindRoute.cs
+++ across-the-grapevine/Assets/Scripts/FindRoute.cs
@@ -7,12 +7,13 @@ public class FindRoute : MonoBehaviour
 {
     //array is y then x
     bool[,] array2D = new bool[,] { { true, true, true, true }, { false, false, false, true}, { true, false, false, true}, { false, false, false, true} };
-    public Tilemap tilemap;
+    public ITilemap tilemap;
     public Vector3 GrapePos;
     public Vector3 OldPos;
     public Vector3 Vec;
     public bool moving;
-    
+
+
     void Start() {
         Vec.y = -1.5f;
         Vec.x = -1.5f;
@@ -21,9 +22,9 @@ public class FindRoute : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(Move());
+        StartCoroutine(MoveVertical());
     }
-    IEnumerator Move()
+    IEnumerator MoveVertical()
     {
         if (moving == false) {
             moving = true;
@@ -38,18 +39,6 @@ public class FindRoute : MonoBehaviour
                 Vec.y -= 1f;
                 OldPos = GrapePos;
                 GrapePos.y = GrapePos.y - 1f;
-            }
-            else if (GrapePos.x + 1 < 4 && array2D[(int)GrapePos.x + 1, (int)GrapePos.y] == true && GrapePos.x + 1 != OldPos.x)
-            {
-                Vec.x += 1f;
-                OldPos = GrapePos;
-                GrapePos.x = GrapePos.x + 1f;
-            }
-            else if (GrapePos.x - 1 > -1 && array2D[(int)GrapePos.x - 1, (int)GrapePos.y] == true && GrapePos.x - 1 != OldPos.x)
-            {
-                Vec.x -= 1f;
-                OldPos = GrapePos;
-                GrapePos.x = GrapePos.x - 1f;
             }
             yield return new WaitForSeconds(2.0f);
             transform.localPosition = Vec;
