ec060a1cea3b68734fe80d4414d548932d81cb62
Dexo55 <Dexoh53@gmail.com>
2022-11-20 11:58:27 -0500

    Added all tile types for grape pathing

--- across-the-grapevine/Assets/Scripts/FindRoute.cs
+++ across-the-grapevine/Assets/Scripts/FindRoute.cs
@@ -12,7 +12,7 @@ public class FindRoute : MonoBehaviour
     //  1   5
     //  0   4
     //change to int array with numbers repesenting tile type
-    int[,] array2D = new int[,] { {5, 3, 0, 0}, {6, 4, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0} };
+    int[,] array2D = new int[,] { {7, 7, 7, 7}, {0, 0, 0, 2}, {0, 0, 0, 2}, {0, 0, 0, 2} };
     public ITilemap tilemap;
     public Vector3 GrapePos;
     public Vector3 OldPos;
@@ -35,69 +35,149 @@ public class FindRoute : MonoBehaviour
             moving = true;
             switch (array2D[(int)GrapePos.x, (int)GrapePos.y]) {
                 case 1: //Vertical Line
-                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
                     }
                     break;
                 case 2: //Horisontal Line
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){ //add numbers to .Contains to add possilbe tiles grape can move to
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){ //add numbers to .Contains to add possilbe tiles grape can move to
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
                     }
                     break;
                 case 3: //Down-Right turn
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x){
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x){
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y){
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y){
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
                     }
                     break;
                 case 4: //Down-Left turn
-                    if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                    if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y){
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y){
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
                     }
                     break;
                 case 5: //Up-Right turn
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
                     }
                     break;
                 case 6: //Up-Left turn
-                    if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
-                        Vec.x -= 1f;
-                        OldPos = GrapePos;
-                        GrapePos.x = GrapePos.x - 1f;
-                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                        Vec.x -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x - 1f;
+                    }
+                    break;
+                case 7: //Right Fork
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                        Vec.x += 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x + 1f;
+                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                        Vec.y += 1f;
+                        OldPos = GrapePos;
+                        GrapePos.y = GrapePos.y + 1f;
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                        Vec.y -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.y = GrapePos.y - 1f;
+                    }
+                    break;
+                case 8: //Left Fork
+                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                        Vec.y += 1f;
+                        OldPos = GrapePos;
+                        GrapePos.y = GrapePos.y + 1f;
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                        Vec.x -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x - 1f;
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                        Vec.y -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.y = GrapePos.y - 1f;
+                    }
+                    break;
+                case 9: //Down Fork
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                        Vec.x += 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x + 1f;
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                        Vec.x -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x - 1f;
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                        Vec.y -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.y = GrapePos.y - 1f;
+                    }
+                    break;
+                case 10: //Up Fork
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                        Vec.x += 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x + 1f;
+                    } else  if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                        Vec.x -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x - 1f;
+                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                        Vec.y += 1f;
+                        OldPos = GrapePos;
+                        GrapePos.y = GrapePos.y + 1f;
+                    }
+                    break;
+                case 11: //Crossroads
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                        Vec.x += 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x + 1f;
+                    
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                        Vec.x -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.x = GrapePos.x - 1f;
+                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                        Vec.y += 1f;
+                        OldPos = GrapePos;
+                        GrapePos.y = GrapePos.y + 1f;
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                        Vec.y -= 1f;
+                        OldPos = GrapePos;
+                        GrapePos.y = GrapePos.y - 1f;
                     }
                     break;
             }
