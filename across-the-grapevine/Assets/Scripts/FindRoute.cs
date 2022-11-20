863b438bb5ad090e0ed7b7cf4a85e1517265f991
Dexo55 <Dexoh53@gmail.com>
2022-11-20 17:32:44 -0500

    win

--- across-the-grapevine/Assets/Scripts/FindRoute.cs
+++ across-the-grapevine/Assets/Scripts/FindRoute.cs
@@ -34,6 +34,7 @@ public class FindRoute : MonoBehaviour
     public TileBase Crossroads;
     public TileBase START;
     public TileBase END;
+    public TileBase BIRD;
     void Start() {
         Vec.y = -1.5f;
         Vec.x = -1.5f;
@@ -85,6 +86,8 @@ public class FindRoute : MonoBehaviour
                     array2D[i, j] = 10;
                 } else if (currentTile == Crossroads) {
                     array2D[i, j] = 11;
+                } else if (currentTile == BIRD) {
+                    array2D[i, j] = 12;
                 } else if (currentTile == START){
                     array2D[i, j] = 5;
                 } else if (currentTile == END){
@@ -103,11 +106,11 @@ public class FindRoute : MonoBehaviour
             moving = true;
             switch (array2D[(int)GrapePos.x, (int)GrapePos.y]) {
                 case 1: //Vertical Line
-                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
@@ -116,11 +119,11 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 2: //Horisontal Line
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){ //add numbers to .Contains to add possilbe tiles grape can move to
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){ //add numbers to .Contains to add possilbe tiles grape can move to
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
@@ -129,11 +132,11 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 3: //Down-Right turn
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x){
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x){
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y){
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y){
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
@@ -142,11 +145,11 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 4: //Down-Left turn
-                    if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                    if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y){
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y){
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
@@ -155,11 +158,11 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 5: //Up-Right turn
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
@@ -168,11 +171,11 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 6: //Up-Left turn
-                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
-                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
@@ -181,15 +184,15 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 7: //Right Fork
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
@@ -198,15 +201,15 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 8: //Left Fork
-                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
-                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
@@ -215,15 +218,15 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 9: //Down Fork
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
@@ -232,15 +235,15 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 10: //Up Fork
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
-                    } else  if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                    } else  if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
-                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
@@ -249,26 +252,29 @@ public class FindRoute : MonoBehaviour
                     }
                     break;
                 case 11: //Crossroads
-                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
+                    if (GrapePos.x + 1 < 4 && new List<int>{2, 4, 6, 8, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x + 1, (int)GrapePos.y]) && GrapePos.x + 1 != OldPos.x) {
                         Vec.x += 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x + 1f;
                     
-                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
+                    } else if (GrapePos.x - 1 > -1 && new List<int>{2, 3, 5, 7, 9, 10, 11, 12}.Contains(array2D[(int)GrapePos.x - 1, (int)GrapePos.y]) && GrapePos.x - 1 != OldPos.x){
                         Vec.x -= 1f;
                         OldPos = GrapePos;
                         GrapePos.x = GrapePos.x - 1f;
-                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
+                    } else if (GrapePos.y + 1 < 4 && new List<int>{1, 3, 4, 7, 8, 9, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y + 1]) && GrapePos.y + 1 != OldPos.y) {
                         Vec.y += 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y + 1f;
-                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
+                    } else if (GrapePos.y - 1 > -1 && new List<int>{1, 5, 6, 7, 8, 10, 11, 12}.Contains(array2D[(int)GrapePos.x, (int)GrapePos.y - 1]) && GrapePos.y - 1 != OldPos.y) {
                         Vec.y -= 1f;
                         OldPos = GrapePos;
                         GrapePos.y = GrapePos.y - 1f;
                     } else {
                         OldPos = RealPos;
                     }
+                    break;
+                case 12: //Obsticles
+                    GameStart.lose = true;
                     break;
             }
             transform.localPosition = Vec;
