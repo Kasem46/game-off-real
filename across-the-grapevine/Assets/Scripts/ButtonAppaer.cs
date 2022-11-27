8698fe6491ce4c2fadff0d43ba510bdaf3795814
Dexo55 <Dexoh53@gmail.com>
2022-11-27 12:40:04 -0500

    prepared for beta

---
+++ across-the-grapevine/Assets/Scripts/ButtonAppaer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonAppaer : MonoBehaviour {
+    public Image next;
+    public Text next2;
+    // Start is called before the first frame update
+    void Start() {
+        next.enabled = false;
+        next2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (GameStart.win == true) {
+            next.enabled = true;
+            next2.enabled = true;
+        } else {
+            next.enabled = false;
+            next2.enabled = false;
+        }
+    }
+}
